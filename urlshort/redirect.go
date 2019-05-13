793a83dbb4d001982f0f697f1e4f143f47ec9121
Ted Summer <ted.summer2@gmail.com>
2019-05-12 22:19:28 -0500

    refactor(redirect): move redirect to sepearte file

--- urlshort/redirect.go
+++ urlshort/redirect.go
@@ -1,76 +1,7 @@
 package urlshort
 
-import (
-	"encoding/json"
-	"errors"
-	"io/ioutil"
-)
-
 type Redirect struct {
 	Src  string `json:"src"`
 	Dest string `json:"dest"`
 	Ttl  int    `json:"ttl"`
-}
-
-type RedirectRegistry struct {
-	Redirects map[string]Redirect
-}
-
-// Create a new registry
-func NewRedirectRegistry() *RedirectRegistry {
-	var redirectReg RedirectRegistry
-	redirectReg.Redirects = make(map[string]Redirect)
-	return &redirectReg
-}
-
-// Get a redirect with the provided source path
-func (redReg *RedirectRegistry) Get(src string) (Redirect, bool) {
-	redirect, ok := redReg.Redirects[src]
-	return redirect, ok
-}
-
-// Add a Redirect to the registry
-func (redReg *RedirectRegistry) Add(redirect *Redirect) error {
-	_, ok := redReg.Get(redirect.Src)
-	if ok {
-		return errors.New("Source is already mapped")
-	}
-	redReg.Redirects[redirect.Src] = *redirect
-	return nil
-}
-
-// Add a slice of Redirects to registry
-func (redReg *RedirectRegistry) AddFromSlice(redirects []Redirect) error {
-	for _, redirect := range redirects {
-		err := redReg.Add(&redirect)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// Add to registry by loading json array from file
-func (redReg *RedirectRegistry) AddFromJSON(jsonPath string) error {
-	if jsonPath == "" {
-		return nil
-	}
-
-	// get json into slice of Redirects
-	jsonRedirects, err := ioutil.ReadFile(jsonPath)
-	if err != nil {
-		return err
-	}
-	redirects := make([]Redirect, 0)
-	err = json.Unmarshal(jsonRedirects, &redirects)
-	if err != nil {
-		return err
-	}
-
-	// add redirects to the registry
-	err = redReg.AddFromSlice(redirects)
-	if err != nil {
-		return err
-	}
-	return nil
 }
